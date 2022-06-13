6d009b6bd6754d3048bcf216d7a8d92bd44c0cc9
JerryRC18 <55813679+JerryRC18@users.noreply.github.com>
2022-06-13 16:16:46 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios);
 
-axios.defaults.baseURL = 'https://62a79b0fd3177a1ede705ba5--meek-malabi-4f7c74.netlify.app/:3000';
+axios.defaults.baseURL = 'https://proyectolenguajes.herokuapp.com/:80';
 
 new Vue({
   router,
