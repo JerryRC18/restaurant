431a9c571c6357c2f00d93a050626278bd601af9
Gerardo <jesusgrobles9@gmail.com>
2022-06-12 22:49:03 -0500

    conexion host sql

--- server/connection.js
+++ server/connection.js
@@ -4,11 +4,18 @@ const { promisify } = require('util');
 
 //Configurar la conexion a la base de datos 
 const connection = mysql.createPool({
+    host: 'bqsvzdgzbj1smi1rgdnr-mysql.services.clever-cloud.com',
+    user: 'ubnz3y3wcnbqvk4s',
+    password: 'c7R8Xtm1hP3C2KFSqqdZ',
+    database: 'bqsvzdgzbj1smi1rgdnr'
+});
+
+/*const connection = mysql.createPool({
     host: 'localhost',
     user: 'proyectofinal',
     password: 'GERARDO180999',
     database: 'bar_db'
-});
+});*/
 
 //Mas conection 
 //Funcion que solo se usa localmente 
